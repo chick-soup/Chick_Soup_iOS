f60c7b1689899c00fcc2edd88135572e2a29fa9f
DohyunKIm <rlaehgus1229@gmail.com>
2020-01-02 17:15:46 +0900

    Add FriendHideList

---
+++ Kakaotalk_CloneCoding/FriendHideList.swift
@@ -0,0 +1,16 @@
+//
+//  FriendHideList.swift
+//  Kakaotalk_CloneCoding
+//
+//  Created by DohyunKim on 2020/01/02.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var friendHideListPhotoID: String = ""
+
+class friendHideListCell: UITableViewCell {
+    
+}
