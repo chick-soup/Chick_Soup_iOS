d845ee9f48ab095dce3740438c2f6c5c5501f2c9
DohyunKIm <rlaehgus1229@gmail.com>
2020-01-02 17:17:54 +0900

    Add HiddenManageVC

---
+++ Kakaotalk_CloneCoding/HiddenManageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HiddenManageViewController.swift
+//  Kakaotalk_CloneCoding
+//
+//  Created by DohyunKim on 2020/01/02.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class HiddenManageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
